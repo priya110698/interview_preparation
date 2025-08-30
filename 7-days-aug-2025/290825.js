02302162e24e2a830a6379e51e61ef5fc5c68b8d
priya110698 <priyanka980611@gmail.com>
2025-08-30 13:12:35 +0530

    workout

--- 7-days-aug-2025/290825.js
+++ 7-days-aug-2025/290825.js
@@ -12,8 +12,34 @@ function fibo(n) {
     return val;
 }
 
-console.log(fibo(5)); //5
-console.log(fibo(7)); //13
-console.log(fibo(10)); //55
-console.log(fibo(9)); //34
+// console.log(fibo(5)); //5
+// console.log(fibo(7)); //13
+// console.log(fibo(10)); //55
+// console.log(fibo(9)); //34
 
+// Largest & smallest in array
+
+function smallLargeM1(arr) {
+    return Math.min(...arr) + ' - ' + Math.max(...arr);
+}
+
+// console.log(smallLargeM1([9, 1, 2, 63, 5, 23, 7, 35, 4, 4])); 
+
+
+
+function smallLargeM2(arr) {
+    let temp = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    return arr[0] + ' - ' + arr[arr.length - 1];
+}
+
+console.log(smallLargeM2([9, 1, 2, 63, 5, 23, 7, 35, 4, 4])); 
